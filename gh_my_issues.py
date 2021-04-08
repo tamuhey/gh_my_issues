5592d84cca603120beb29a701e9a3d679b0ff018
Yohei Tamura <tamuhey@gmail.com>
2021-04-08 17:16:34 +0900

    add repo info

--- gh_my_issues.py
+++ gh_my_issues.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3.9
-
 from dataclasses import dataclass
 import json
+import os
 import pprint
 import subprocess
 from subprocess import PIPE
 import sys
-from typing import Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, List, Optional, Tuple, Union
 
 import fire
+
+
+@dataclass
+class Repository:
+    owner: str
+    name: str
+
+    @classmethod
+    def from_resp(cls, dat: dict[str, Any]) -> "Repository":
+        owner = dat["owner"]["name"]
+        name = dat["name"]
+        return cls(owner, name)
+
+    def __str__(self) -> str:
+        return f"{self.owner}/{self.name}"
 
 
 @dataclass
@@ -16,17 +31,20 @@ class Issue:
     created_at: str
     title: str
     url: str
+    repo: Repository
 
     @classmethod
-    def from_resp(cls, dat: dict[str, str]) -> "Issue":
+    def from_resp(cls, dat: dict[str, Any]) -> "Issue":
         created_at = dat["createdAt"]
         title = dat["title"]
         url = dat["url"]
-        return Issue(created_at=created_at, title=title, url=url)
+        repo = Repository.from_resp(dat["repository"])
+        return Issue(created_at=created_at, title=title, url=url, repo=repo)
 
     def __str__(self) -> str:
         return f"""
 Title      : {self.title}
+Repo       : {self.repo}
 Created at : {self.created_at}
 url        : {self.url}
 """
@@ -42,7 +60,8 @@ issues: list[Issue] = []
 
 
 def _list():
-    cmd = """gh api graphql -f query='
+    cmd = """
+gh api graphql -f query='
         query {
         search(first: 100, type: ISSUE, query: "assignee:tamuhey is:open") {
             issueCount
@@ -56,11 +75,24 @@ def _list():
                 createdAt
                 title
                 url,
+                bodyText,
+                repository {
+                    owner {
+                        ... on Organization {
+                            name
+                        }
+                        ... on User {
+                            name: login
+                        }
+                    },
+                    name,
+                },
                 }
             }
             }
         }
-        }'"""
+        }'
+    """
     ret = subprocess.run(cmd, shell=True, stdout=PIPE, check=True)
     dat = json.loads(ret.stdout.decode())
 
@@ -93,14 +125,23 @@ def cmd_detail(index: Optional[int] = None):
     print(str(issues[index]))
 
 
+def cmd_new():
+    user = os.environ["GITHUB_USERNAME"]
+    repo = os.environ["TODO_REPO"]
+    subprocess.run(
+        ["gh", "--repo", repo, "issue", "create", "--assignee", user],
+        check=True,
+    )
+
+
 def ask(msg: Optional[str] = None) -> bool:
     msg = msg or "Are you ok? (y/N): "
     rep = input(msg).lower()
     return bool(rep) and rep[0] == "y"
 
 
-CMDs = {"list": cmd_list, "close": cmd_close, "detail": cmd_detail}
-ALIASES = {"l": "list", "c": "close"}
+CMDs = {"list": cmd_list, "close": cmd_close, "detail": cmd_detail, "new": cmd_new}
+ALIASES = {"l": "list", "c": "close", "n": "new"}
 
 
 def read_cmd() -> Union[tuple[Callable, List[str]], str]:
